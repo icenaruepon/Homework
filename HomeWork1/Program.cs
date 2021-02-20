0778a236437f8ba04250eda420722151344b8218
LAPTOP-CDM690QG\ACER <naruepon.ice@mail.kmutt.ac.th>
2021-02-20 17:43:14 +0700

    add input & split password

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 6 digit password : ");
+            int pass = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input your agency : ");
+            string agency = Console.ReadLine();
+
+
+
+
+        }
+    }
+}
